117088d825ab5d3f54b6fa89f6906366435992ca
Marianna Dudzinska <MDudzinska@ppg.com>
2022-01-18 16:34:46 +0100

    fix

--- src/app/forms/rpg-form/rpg-form.component.ts
+++ src/app/forms/rpg-form/rpg-form.component.ts
@@ -17,17 +17,6 @@ export class RpgFormComponent {
       player: new FormControl(''),
       race: new FormControl(''),
       origin: new FormControl(''),
-    }),
-
-    weapon1: new FormGroup({
-      weaponName1: new FormControl(''),
-      weaponType1: new FormControl(''),
-      range1: new FormControl(''),
-      damage1: new FormControl(''),
-      specialDamage1: new FormControl(''),
-    }),
-
-
+    })
   });
-
 }
